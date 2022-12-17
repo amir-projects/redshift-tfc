4833a019317ab23ff41d4536a5ec126a700f74bb
amir-projects <humairabinte.amir@gmail.com>
2022-12-17 23:57:05 +0600

    redshift_cluster_deploy

--- resource.tf
+++ resource.tf
@@ -116,6 +116,8 @@ resource "aws_iam_role_policy" "s3_full_access_policy" {
   })
 }
 
+##We are creating an IAM role using the policy which we just defined. We’ll attach this Role to the cluster later.##
+
 resource "aws_iam_role" "redshift_role" {
   name = "redshift_role"
 
@@ -132,4 +134,40 @@ resource "aws_iam_role" "redshift_role" {
       },
     ]
   })
+}
+
+###Finally, we’ll define the redshift cluster. We will define the password in terraform.tfvars. We will use this password, When we connect to the cluster##
+
+resource "aws_redshift_cluster" "default" {
+
+  cluster_identifier = var.rs_cluster_identifier
+
+  database_name = var.rs_database_name
+
+  master_username = var.rs_master_username
+
+  master_password = var.rs_master_pass
+
+  node_type = var.rs_nodetype
+
+  cluster_type = var.rs_cluster_type
+
+  cluster_subnet_group_name = aws_redshift_subnet_group.redshift_subnet_group.id
+
+  skip_final_snapshot = true
+
+  iam_roles = ["${aws_iam_role.redshift_role.arn}"]
+
+  depends_on = [
+
+    aws_vpc.redshift_vpc,
+
+    aws_default_security_group.redshift_security_group,
+
+    aws_redshift_subnet_group.redshift_subnet_group,
+
+    aws_iam_role.redshift_role
+
+  ]
+
 }
--- variable.tf
+++ variable.tf
@@ -4,5 +4,12 @@ variable "vpc_cidr" {}
 
 #Subnet variables#
 variable "redshift_subnet_cidr_first" {}
-
 variable "redshift_subnet_cidr_second" {}
+
+##Redshift Clusters##
+variable "rs_cluster_identifier" {}
+variable "rs_database_name" {}
+variable "rs_master_username" {}
+variable "rs_master_pass" {}
+variable "rs_nodetype" {}
+variable "rs_cluster_type" {}
